aaffbde322f989196db260d0c144e9a39afb157e
chaithu.vtalent <chaithu.vtalent@gmail.com>
2019-05-01 13:30:44 +0530

    new sample class created

---
+++ ConsolApp1/ConsolApp1/sample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolApp1
+{
+    class sample
+    {
+        public int add()
+        {
+            int x = 10;
+            int y = 20;
+            int sum = x + y;
+            return sum;
+        }
+    }
+}
